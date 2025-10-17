045ce078e24fe43e99ed80001898b7bb64023054
Minokainduwara <minokainduwara@gmail.com>
2025-10-18 02:49:56 +0530

    error fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
-import SignUpintro from "./pages/signUpintro.jsx"
+
 import HomePage from "./pages/HomePage.jsx"
-import StatsSection from "./components/StatsSection.jsx"
-import AnimatedNavbar from "./components/navbar.jsx"
-import AboutUs from "./pages/AboutUs.jsx"
+
 
 function App() {
   return (
