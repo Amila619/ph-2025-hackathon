f1540e796ff0fa9dbad4308c8a565c7f1e32ed36
Amila619 <amilasandeepa349@gmail.com>
2025-10-17 19:13:27 +0530

    - creted some react components and added react router base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,34 @@
+// import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Navigate } from 'react-router-dom'
+
 function App() {
+
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path='/' element={<RootLayout />}>
+  //       <Route index element={!isAuthenticated ? <Home /> : <Navigate to="/dashboard" replace />} />
+  //       {!isAuthenticated && (<><Route path='login' element={<AdminLogin />} />
+  //         <Route path='reg' element={<RegisterLayout />}>
+  //           <Route index element={<Navigate to="team" replace />} />
+  //           <Route path='team' element={<RegisterTeam />} />
+  //           <Route path='user/:id' element={<RegisterUser />} />
+  //         </Route>
+  //         <Route path='teams' element={<Teams />} /></>)}
+  //       <Route element={<ProtectedRoute />}>
+  //         <Route path='dashboard' element={<AdminDashboard />} />
+  //         <Route path='update_team/:id' element={<UpdateTeam />} />
+  //       </Route>
+  //       <Route path='guidelines' element={<GuideLines />} />
+  //       <Route path='agenda' element={<Agenda />} />
+  //       <Route path="*" element={<NotFound />} />
+  //     </Route>
+  //   )
+  // )
+
   return (
-    <>
-    </>
+    <div>
+
+      {/* <RouterProvider router={router} /> */}
+    </div>
   )
 }
 
