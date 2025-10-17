877ee9a9120680461d8e18e7c25c2a2d3c2aa3c1
Minokainduwara <minokainduwara@gmail.com>
2025-10-18 02:39:52 +0530

    App testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,12 @@ import SignUpintro from "./pages/signUpintro.jsx"
 import HomePage from "./pages/HomePage.jsx"
 import StatsSection from "./components/StatsSection.jsx"
 import AnimatedNavbar from "./components/navbar.jsx"
+import AboutUs from "./pages/AboutUs.jsx"
 
 function App() {
   return (
     <>
-      <AnimatedNavbar></AnimatedNavbar>
       <HomePage></HomePage>
-      <StatsSection></StatsSection>
-
-      
     </>
   )
 }
