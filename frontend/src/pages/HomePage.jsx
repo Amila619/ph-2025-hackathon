396c52b3b82802df4368139fff7dfd7694b25c02
Minokainduwara <minokainduwara@gmail.com>
2025-10-18 12:40:19 +0530

    Home Page Updated

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-
 import HeroSection from '../components/HeroSection';
 import MarketplaceTabs from '../components/MarketplaceTabs';
 import FeaturedListings from '../components/FeaturedListings';
 import HowItWorks from '../components/HowLtWorks';
 import WhyChooseUs from '../components/WhyChooseUs';
-import Contact from './Contact';
-import { FloatButton } from 'antd';
 
 export default function HomePage() {
   return (
@@ -28,26 +25,8 @@ export default function HomePage() {
       <section id="why">
         <WhyChooseUs />
       </section>
-      <FloatButton
-        style={{ insetBlockEnd: 175 }}
-        tooltip={{
-          // tooltipProps is supported starting from version 5.25.0.
-          title: 'Since 5.25.0+',
-          color: 'blue',
-          placement: 'top',
-        }}
-      />
-      <FloatButton
-        style={{ insetBlockEnd: 108 }}
-        tooltip={{
-          // tooltipProps is supported starting from version 5.25.0.
-          title: 'Since 5.25.0+',
-          color: 'blue',
-          placement: 'top',
-        }}
-      />
-      <FloatButton tooltip={<div>Documents</div>} />
-      
+
+
     </div>
   );
 }
